a8e7cd9e21740a921858775e360ad952f29b9250
Ilya Rodionov <ris58h@gmail.com>
2022-03-30 11:23:12 +0300

    Remove useless describe from test

--- index.test.js
+++ index.test.js
@@ -1,21 +1,19 @@
 import { transpileText } from './index.js';
 import { strict as assert } from 'assert';
 
-describe('dgaf-js', () => {
-    describe('transpileText', () => {
-        it('Should replace dots', () => {
-            assert.equal(transpileText('x.a'), 'x?.a')
-            assert.equal(transpileText('x.a.b.c'), 'x?.a?.b?.c')
-            assert.equal(transpileText('x.a().b?.c'), 'x?.a()?.b?.c')
-        })
-        it ('Should preserve indentation', () => {
-            const input = `if (x.a) {
-                console.log(x.a)
-            }`
-            const expectedOutput = `if (x?.a) {
-                console?.log(x?.a)
-            }`
-            assert.equal(transpileText(input), expectedOutput)
-        })
+describe('transpileText', () => {
+    it('Should replace dots', () => {
+        assert.equal(transpileText('x.a'), 'x?.a')
+        assert.equal(transpileText('x.a.b.c'), 'x?.a?.b?.c')
+        assert.equal(transpileText('x.a().b?.c'), 'x?.a()?.b?.c')
+    })
+    it ('Should preserve indentation', () => {
+        const input = `if (x.a) {
+            console.log(x.a)
+        }`
+        const expectedOutput = `if (x?.a) {
+            console?.log(x?.a)
+        }`
+        assert.equal(transpileText(input), expectedOutput)
     })
 })
