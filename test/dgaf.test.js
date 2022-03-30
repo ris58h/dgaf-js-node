2ffa22d79c8423f2772c924eca5648e9788f350a
Ilya Rodionov <ris58h@gmail.com>
2022-03-30 14:04:13 +0300

    fix tests

--- test/dgaf.test.js
+++ test/dgaf.test.js
@@ -5,23 +5,22 @@ describe('transpile', () => {
     it('Should replace member access', () => {
         assert.equal(transpile('x.a'), 'x?.a')
         assert.equal(transpile('x.a.b.c'), 'x?.a?.b?.c')
-        assert.equal(transpile('x.a().b?.c'), 'x?.a()?.b?.c')
+    })
+    it("Shouldn't replace member access in left-side of assignment", () => {
+        assert.equal(transpile('x.a = y'), 'x.a = y')
+        assert.equal(transpile('x.a = y.b'), 'x.a = y?.b')
+    })
+    it('Should replace function call', () => {
+        assert.equal(transpile('foo()'), 'foo?.()')
+        assert.equal(transpile('foo()()'), 'foo?.()?.()')
     })
     it('Should preserve indentation', () => {
         const input = `if (x.a) {
             console.log(x.a)
         }`
         const expectedOutput = `if (x?.a) {
-            console?.log(x?.a)
+            console?.log?.(x?.a)
         }`
         assert.equal(transpile(input), expectedOutput)
-    })
-    it("Shouldn't replace member access in left-side of assignment", () => {
-        assert.equal(transpile('x.a = y'), 'x.a = y')
-        assert.equal(transpile('x.a = y.b'), 'x.a = y?.b')
-    })
-    it.only('Should replace function call', () => {
-        assert.equal(transpile('foo()'), 'foo?.()')
-        assert.equal(transpile('foo.bar()'), 'foo?.bar?.()')
     })
 })
