bce28fa5d42a6800ed6ae2be60da44f65b71f072
Ilya Rodionov <ris58h@gmail.com>
2022-03-30 16:01:01 +0300

    Parse cmd args

--- index.js
+++ index.js
@@ -3,16 +3,39 @@
 const transpile = require('./dgaf').transpile
 const fs = require("fs")
 
-if (process.argv.length < 3) {
-    console.error('No input file specified')
-    process.exit(1)
+if (process.argv.length === 5 && process.argv[2] === '-t' && process.argv[3] === '-c') {
+    transpileText(process.argv[4])
+} else if (process.argv.length === 4 && process.argv[2] === '-t') {
+    readFile(process.argv[3], transpileText)
+} else if (process.argv.length === 4 && process.argv[2] === '-c') {
+    interpretText(process.argv[3])
+} else if (process.argv.length === 3) {
+    readFile(process.argv[2], interpretText)
+} else if (process.argv.length === 2) {
+    repl()
+} else {
+    console.error("Illegal arguments")
 }
 
-fs.readFile(process.argv[2], 'utf8', (text, error) => {
-    if (error) {
-        console.error(error)
-        return
-    }
+function repl() {
+    console.error("TODO repl")
+}
+
+function interpretText(text) {
+    console.error("TODO interpretText: " + text)
+}
+
+function transpileText(text) {
     const transpiledText = transpile(text)
     console.log(transpiledText)
-})
+}
+
+function readFile(path, callback) {
+    fs.readFile(path, 'utf8', (data, error) => {
+        if (error) {
+            console.error(error)
+            return
+        }
+        callback(data)
+    })
+}
