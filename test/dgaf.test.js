f3a6c02142d8708b595ab174c0cdcc0204b11e99
Ilya Rodionov <ris58h@gmail.com>
2022-03-31 12:13:43 +0300

    nore tests

--- test/dgaf.test.js
+++ test/dgaf.test.js
@@ -3,31 +3,36 @@ const assert = require('assert').strict
 
 describe('transpile', () => {
     it('Should replace dot member access', () => {
-        assert.equal(transpile('x.a'), 'x?.a')
-        assert.equal(transpile('x.a.b.c'), 'x?.a?.b?.c')
+        assert.equal(transpile('foo.bar'), 'foo?.bar')
+        assert.equal(transpile('foo.bar.baz'), 'foo?.bar?.baz')
     })
-    it("Shouldn't replace dot member access in the left-side of assignment", () => {
-        assert.equal(transpile('x.a = y'), 'x.a = y')
-        assert.equal(transpile('x.a = y.b'), 'x.a = y?.b')
+    it("Shouldn't replace dot member access in the left side of assignment", () => {
+        assert.equal(transpile('foo.bar = baz'), 'foo.bar = baz')
+        assert.equal(transpile('foo.bar = baz.bar'), 'foo.bar = baz?.bar')
     })
     it('Should replace bracket member access', () => {
         assert.equal(transpile('foo["bar"]'), 'foo?.["bar"]')
-        assert.equal(transpile('foo["bar"]["baz"]'), 'foo?.["bar"]?.["baz"]')
+        assert.equal(transpile('foo["bar"][baz][777]'), 'foo?.["bar"]?.[baz]?.[777]')
     })
-    it("Shouldn't replace bracket member access in the left-side of assignment", () => {
-        assert.equal(transpile('x["a"] = y'), 'x["a"] = y')
-        assert.equal(transpile('x["a"] = y["b"]'), 'x["a"] = y?.["b"]')
+    it("Shouldn't replace other bracket expressions", () => {
+        assert.equal(transpile('var foo = []'), 'var foo = []')
+        assert.equal(transpile('var foo = ["bar"]'), 'var foo = ["bar"]')
+        // assert.equal(transpile('var foo = {[bar] = "baz"}'), 'var foo = {[bar] = "baz"}') //TODO 
+    })
+    it("Shouldn't replace bracket member access in the left side of assignment", () => {
+        assert.equal(transpile('foo["bar"] = baz'), 'foo["bar"] = baz')
+        assert.equal(transpile('foo["bar"] = baz["bar"]'), 'foo["bar"] = baz?.["bar"]')
     })
     it('Should replace function call', () => {
         assert.equal(transpile('foo()'), 'foo?.()')
         assert.equal(transpile('foo()()'), 'foo?.()?.()')
     })
     it('Should preserve indentation', () => {
-        const input = `if (x.a) {
-            console.log(x.a)
+        const input = `if (foo.bar) {
+            console.log(foo.bar)
         }`
-        const expectedOutput = `if (x?.a) {
-            console?.log?.(x?.a)
+        const expectedOutput = `if (foo?.bar) {
+            console?.log?.(foo?.bar)
         }`
         assert.equal(transpile(input), expectedOutput)
     })
