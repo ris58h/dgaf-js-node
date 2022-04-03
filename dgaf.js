78269278aca5ce36b0be0b62acc47e8b6acd49c4
Ilya Rodionov <ris58h@gmail.com>
2022-04-03 15:04:40 +0300

    processNode

--- dgaf.js
+++ dgaf.js
@@ -4,64 +4,70 @@ const JavaScript = require('tree-sitter-javascript')
 exports.transpile = function(text) {
     const parser = new Parser()
     parser.setLanguage(JavaScript)
-
     const tree = parser.parse(text)
     // printNode(tree.rootNode) //TODO
 
     const replacements = []
-    walkTree(tree, node => {
-        if (node.type === 'identifier') {
-            const accessIndentifier = !node.nextSibling
-                || node.nextSibling.type === ';'
-                || isDotMemeberAccess(node.nextSibling)
-                || isBracketMemberAccess(node.nextSibling)
-                || isCallArguments(node.nextSibling)
-                || isCallArgument(node)
-                || isInParentheses(node)
-                || isUpdateArgument(node)
-                || isBinaryExpressionArgument(node)
-                || isOptionalChaining(node.nextSibling)
-                || isForEnumerable(node)
-                || isArrayElement(node)
-                || isPairRightSide(node)
-            if (accessIndentifier && isInAccessChain(node)) {
-                const identifier = node.text
-                const replaceWith = `(typeof ${identifier} === "undefined" ? void 0 : ${identifier})`
-                addReplacement(node.startIndex, node.endIndex, replaceWith)
-            } else if (isLeftSideOfAugmentedAssignment(node)) {
-                const identifier = node.text
-                const replaceWith = `if (typeof ${identifier} !== "undefined") {${identifier}`
-                addReplacement(node.startIndex, node.endIndex, replaceWith)
-                addReplacement(node.parent.endIndex, node.parent.endIndex, '}')
-            }
-        } else if (isDotMemeberAccess(node) && !isInLeftSideOfAssignment(node.parent)) {
-            addReplacement(node.startIndex, node.endIndex, '?.')
-        } else if (isBracketMemberAccess(node) && !isInLeftSideOfAssignment(node.parent) && !isOptionalAccess(node)) {
-            addReplacement(node.startIndex, node.startIndex, '?.')
-        } else if (isCallArguments(node) && !isInErrorBranch(node) && !isOptionalAccess(node)) {
-            addReplacement(node.startIndex, node.startIndex, '?.')
-        }
-    })
-
+    walkTree(tree, node => processNode(node, replacements))
     replacements.sort((a, b) => a.from - b.from)
 
     return replace(text, replacements)
+}
+
+function processNode(node, replacements) {
+    if (isReference(node) && isReferencePlace(node)) {
+        processReferenceNode(node)
+        return false
+    } else if (isLeftSideOfAugmentedAssignment(node)) {
+        const identifier = node.text
+        const replaceWith = `if (typeof ${identifier} !== "undefined") {${identifier}`
+        addReplacement(node.startIndex, node.endIndex, replaceWith)
+        addReplacement(node.parent.endIndex, node.parent.endIndex, '}')
+    }
+
+    return true
+
+    function processReferenceNode(node) {
+        if (node.type === 'identifier') {
+            const identifier = node.text
+            const replaceWith = `(typeof ${identifier} === "undefined" ? void 0 : ${identifier})`
+            addReplacement(node.startIndex, node.endIndex, replaceWith)
+        } else if (node.type === 'member_expression') {
+            const dotNode = node.child(1)
+            addReplacement(dotNode.startIndex, dotNode.endIndex, '?.')
+            processReferenceNode(node.child(0))
+        } else if (node.type === 'subscript_expression') {
+            const secondChild = node.child(1)
+            if (secondChild.type === '[') {
+                addReplacement(secondChild.startIndex, secondChild.startIndex, '?.')
+            }
+            processReferenceNode(node.child(0))
+        } else if (node.type === 'call_expression') {
+            const secondChild = node.child(1)
+            if (secondChild.type === 'arguments') {
+                addReplacement(secondChild.startIndex, secondChild.startIndex, '?.')
+                for (const argument of secondChild.namedChildren) {
+                    processReferenceNode(argument)
+                }
+            }
+            processReferenceNode(node.child(0))
+        }
+    }
 
     function addReplacement(from, to, replaceWith) {
         replacements.push({from, to, replaceWith})
     }
 }
 
-function isAccessNode(node) {
-    return ['member_expression', 'subscript_expression', 'call_expression'].includes(node?.type)
+function isReference(node) {
+    return node.type === 'identifier'
+        || node.type === 'member_expression'
+        || node.type === 'subscript_expression'
+        || node.type === 'call_expression'
 }
 
-function isInAccessChain(node) {
-    while (isAccessNode(node.parent)) {
-        node = node.parent
-    }
-    const parentType = node.parent?.type
-    return parentType === 'expression_statement'
+function isReferencePlace(node) {
+    return node.parent?.type === 'expression_statement'
         || isRightSideOfAssignment(node)
         || isCallArgument(node)
         || isInParentheses(node)
@@ -107,11 +113,6 @@ function isUpdateArgument(node) {
     return node.parent?.type === 'update_expression'
 }
 
-function isCallArguments(node) {
-    return node.type === 'arguments'
-        && node.parent?.type === 'call_expression'
-}
-
 function isCallArgument(node) {
     return node.parent?.type === 'arguments'
 }
@@ -120,46 +121,8 @@ function isInParentheses(node) {
     return node.parent?.type === 'parenthesized_expression'
 }
 
-function isDotMemeberAccess(node) {
-    return node.type === '.'
-        && node.parent?.type === 'member_expression'
-}
-
-function isOptionalChaining(node) {
-    return node.type === '?.' && isAccessNode(node.parent)
-}
-
-function isOptionalAccess(node) {
-    return node.previousSibling && isOptionalChaining(node.previousSibling)
-}
-
-function isBracketMemberAccess(node) {
-    return node.type === '['
-        && node.parent?.type === 'subscript_expression'
-}
-
-function isInLeftSideOfAssignment(node) {
-    while (node.parent) {
-        if (node.parent.type === 'assignment_expression') {
-            return !node.previousSibling
-        }
-        node = node.parent
-    }
-    return false
-}
-
 function isRightSideOfAssignment(node) {
     return node.parent?.type === 'assignment_expression' && node.nextSibling?.type !== '='
-}
-
-function isInErrorBranch(node) {
-    while (node.parent) {
-        if (node.parent.hasError()) {
-            return true
-        }
-        node = node.parent
-    }
-    return false
 }
 
 function replace(text, sortedReplacements) {
@@ -177,8 +140,11 @@ function replace(text, sortedReplacements) {
 function walkTree(tree, callback) {
     const cursor = tree.walk()
     while (true) {
-        callback(cursor.currentNode)
-        if (!goDown() && !gotoNextBranch()) {
+        const skipSubtree = !callback(cursor.currentNode)
+        if (!skipSubtree && goDown()) {
+            continue
+        }
+        if (!gotoNextBranch()) {
             return
         }
     }
