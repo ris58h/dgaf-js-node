4970c40338d0a70bbd5e04e01efbe27361865f0a
Ilya Rodionov <ris58h@gmail.com>
2022-04-05 20:59:27 +0300

    fix bin

--- bin/node.js
+++ bin/node.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const transpile = require('../dgaf').transpile
+const transpile = require('..').transpile
 const fs = require("fs")
 const Module = require('module')
 
--- bin/transpiler.js
+++ bin/transpiler.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const transpile = require('../dgaf').transpile
+const transpile = require('..').transpile
 const fs = require("fs")
 
 if (process.argv.length === 4 && process.argv[2] === '-c') {
